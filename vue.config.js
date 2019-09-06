3078dab8cc04659ea28a3fdf92aebe4e44224e80
blueskyawen <bluesky.liu@163.com>
2019-09-07 02:30:41 +0800

    init vc-cat

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  baseUrl: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-lib/' : '/',
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  css: { extract: false },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', path.resolve('examples'))
+      .set('~', path.resolve('packages'))
+
+    config.module
+      .rule('js')
+      .include.add(/packages/).end()
+      .include.add(/examples/).end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap(options => {
+        // 修改它的选项...
+        return options
+      })
+  }
+}
