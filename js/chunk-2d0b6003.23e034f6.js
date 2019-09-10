5af28c340105c05db378326dfeef91045aa9548d
blueskyawen <bluesky.liu@163.com>
2019-09-11 00:57:00 +0800

    Vcg: Update live demo

--- js/chunk-2d0b6003.23e034f6.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b6003"],{"1afc":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",[t("h1",[e._v("单选框")]),t("p",[e._v("单选框radio，用于表单的单选项目")])])}],u=t("2877"),a={},l=Object(u["a"])(a,c,r,!1,null,null,null);n["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d0b6003.23e034f6.js.map
