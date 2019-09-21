1095d493f5f2ba201ebe0fd34339b7746b50f926
blueskyawen <bluesky.liu@163.com>
2019-09-21 13:17:49 +0800

    Vcg: Update live demo

---
+++ js/chunk-2d0b1c47.531a1f3c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b1c47"],{"20f9":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",[t("h1",[e._v("柱状图")]),t("p",[e._v("直观的向用户提供了一种多个项目数据对比呈现方式")])])}],u=t("2877"),l={},s=Object(u["a"])(l,c,r,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0b1c47.531a1f3c.js.map
